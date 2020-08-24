9783fb3beb34dc7f46df9ef37f40a21b3a20d6ad
aunpyz <aunnop_kattiyanet@hotmail.com>
2020-08-25 00:10:36 +0700

    Change to command line binary

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,35 @@
+use std::env;
 use text_on_png::{FontOptions, TextArea};
 
 fn main() {
-    let image: &str = "meme.png";
-    let font_options: FontOptions = FontOptions::new(12, "sans-serif");
-    let text_area: TextArea = TextArea::new(44, 166, 109, 150);
-    let message: &str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-    let output: &str = "file.png";
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 10 {
+        panic!("Number of arguments are not satisfied")
+    }
+
+    let image: &str = &args[1];
+    let font_options: FontOptions = FontOptions::new(
+        (&args[2].parse())
+            .clone()
+            .expect("This argument must be number convertible, parsing font size filed"),
+        &args[3],
+    );
+    let text_area: TextArea = TextArea::new(
+        (&args[4].parse())
+            .clone()
+            .expect("This argument must be number convertible, parsing x-coord failed"),
+        (&args[5].parse())
+            .clone()
+            .expect("This argument must be number convertible, parsing y-coord failed"),
+        (&args[6].parse())
+            .clone()
+            .expect("This argument must be number convertible, parsing width failed"),
+        (&args[7].parse())
+            .clone()
+            .expect("This argument must be number convertible, parsing height failed"),
+    );
+    let message: &str = &args[8];
+    let output: &str = &args[9];
 
     match text_on_png::create(image, font_options, text_area, message, output) {
         Ok(_) => println!("{} created", output),
